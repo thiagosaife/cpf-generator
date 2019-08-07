d80e003a2e150b2b09dd81eaeb8d0e130b23b52e
Thiago Saife Rodrigues <thiago.rodrigues@THIAGORODRIGUES.local>
2019-08-07 14:55:54 -0300

    Enhance the modulo calculation

--- CpfGenerator.js
+++ CpfGenerator.js
@@ -5,25 +5,21 @@ class CpfGenerator {
 
   generateNumbers() {
     const random = number => Math.round(Math.random() * number);
-    const mod = (dividend, divider) =>
-      Math.round(dividend - (Math.floor(dividend / divider) * divider));
     const rn = [...Array(9)].map(() => random(9));
     let m1 = 1;
     let m2 = 2;
-    let d1 = rn.reduceRight((total, number) => {
+    let d1 = rn.reduceRight((total, item) => {
       m1 += 1;
-      return total + (number * m1);
+      return total + (item * m1);
     }, 0);
-    let d2 = rn.reduceRight((total, number) => {
+    let d2 = rn.reduceRight((total, item) => {
       m2 += 1;
-      return total + (number * m2);
+      return total + (item * m2);
     }, 0);
-    d1 = 11 - (mod(d1, 11));
-    if (d1 >= 10) d1 = 0;
+    d1 = (((d1 % 11) < 2) ? 0 : (11 - (d1 % 11)));
 
     d2 += (d1 * 2);
-    d2 = 11 - (mod(d2, 11));
-    if (d2 >= 10) d2 = 0;
+    d2 = (((d2 % 11) < 2) ? 0 : (11 - (d2 % 11)));
 
     if (this.mask) {
       return `${rn[0]}${rn[1]}${rn[2]}.${rn[3]}${rn[4]}${rn[5]}.${rn[6]}${rn[7]}${rn[8]}-${d1}${d2}`;
